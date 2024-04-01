a6befff9fa8fc51a7b164a7a6d1528e20f136e91
RafaelVieiraFelipe <rafael.vf04@gmail.com>
2024-04-01 20:06:48 -0300

    feat: add business crud methods

--- ConstructionFlow.BL/Business/CustomerBusiness.cs
+++ ConstructionFlow.BL/Business/CustomerBusiness.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using ConstructionFlow.Domain.Model;
+using ConstructionFlow.Domain.Payload;
+using ConstructionFlow.Infrastructure.UnitOfWork;
 
 namespace ConstructionFlow.BL.Business
 {
     public class CustomerBusiness
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CustomerBusiness(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CustomerDTO>> GetCustomers()
+        {
+            var customers = await _unitOfWork.CustomerRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<CustomerDTO>>(customers);
+        }
+
+        public CustomerDTO GetCustomer(Guid customerId)
+        {
+            var customer = _unitOfWork.CustomerRepository.Get(x => x.CustomerId == customerId);
+            return _mapper.Map<CustomerDTO>(customer);
+        }
+
+        public Task AddCustomer(CustomerDTO customer)
+        {
+            _unitOfWork.CustomerRepository.Insert(_mapper.Map<Customer>(customer));
+            return _unitOfWork.SaveAsync();
+        }
+
+        public Task UpdateCustomer(CustomerDTO customer)
+        {
+            _unitOfWork.CustomerRepository.Update(_mapper.Map<Customer>(customer));
+            return _unitOfWork.SaveAsync();
+        }
+
+        public Task DeleteCustomer(Guid customerId)
+        {
+            _unitOfWork.CustomerRepository.Delete(customerId);
+            return _unitOfWork.SaveAsync();
+        }
+        
+
     }
 }
--- ConstructionFlow.Infrastructure/UnitOfWork/IGenericRepository.cs
+++ ConstructionFlow.Infrastructure/UnitOfWork/IGenericRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Query;
+﻿using ConstructionFlow.Domain.Model;
+using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
