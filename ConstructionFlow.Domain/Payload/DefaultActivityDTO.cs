cb3319587f7c9371d00ac60bd5a18ad21f66300a
Matheus Alexander <matheusalexalcantara@gmail.com>
2024-04-01 19:17:32 -0300

    feat: create defaultactivity and activity dtos

---
+++ ConstructionFlow.Domain/Payload/DefaultActivityDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionFlow.Domain.Payload
+{
+    public class DefaultActivityDTO
+    {
+        public Guid DefaultActivityId { get; set; }
+        public required string Icon { get; set; }
+        public required string DefaultActivityName { get; set; }
+    }
+}
