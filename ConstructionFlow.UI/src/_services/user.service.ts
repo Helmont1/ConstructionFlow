7c637dc56974f80ee51987e06d398f53f2976b6b
Matheus Alexander <matheusalexalcantara@gmail.com>
2024-04-02 19:30:43 -0300

    feat: added user service and all necessary methods

---
+++ ConstructionFlow.UI/src/_services/user.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { User } from "../_models/user.model";
+
+@Injectable(
+  { providedIn: 'root' }
+)
+export class UserService {
+  url = 'User'
+
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get(this.url);
+  }
+
+  getUser(id: number) {
+    return this.http.get(`${this.url}/${id}`);
+  }
+
+  createUser(user: User) {
+    return this.http.post(this.url, user);
+  }
+
+  editUser(user: User) {
+    return this.http.put(`${this.url}/${user.userId}`, user);
+  }
+
+  deleteUser(id: number) {
+    return this.http.delete(`${this.url}/${id}`);
+  }
+}
