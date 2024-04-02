71b83899eab70c8df26a82a613e2d590833de84a
Renan Maitan <renanmaitan12@gmail.com>
2024-04-02 01:19:41 -0300

    login form

--- ConstructionFlow.UI/src/_pages/login/login.component.ts
+++ ConstructionFlow.UI/src/_pages/login/login.component.ts
@@ -1,15 +1,39 @@
+import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    CommonModule,
+    ReactiveFormsModule,
+    NgIf
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm;
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
 
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+
+  debugEmailErrors() {
+    console.log('Email inválido:', this.loginForm.get('email')?.errors);
+  }
 }
