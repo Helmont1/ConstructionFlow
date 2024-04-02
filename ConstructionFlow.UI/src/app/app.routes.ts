36379978bea18942bf98998d1d5bb14d3715f1b7
RafaelVieiraFelipe <rafael.vf04@gmail.com>
2024-04-01 21:15:18 -0300

    feat: register start

--- ConstructionFlow.UI/src/app/app.routes.ts
+++ ConstructionFlow.UI/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../_pages/home/home.component';
+import { RegisterComponent } from '../_pages/register/register.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'register', component:RegisterComponent}
+
 ];
