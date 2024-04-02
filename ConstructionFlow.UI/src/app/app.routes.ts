68f3212cda5b78c065bd1af96fbdaa32b945dc38
Renan Maitan <renanmaitan12@gmail.com>
2024-04-01 21:13:07 -0300

    login page

--- ConstructionFlow.UI/src/app/app.routes.ts
+++ ConstructionFlow.UI/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../_pages/home/home.component';
+import { LoginComponent } from '../_pages/login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent}
 ];
