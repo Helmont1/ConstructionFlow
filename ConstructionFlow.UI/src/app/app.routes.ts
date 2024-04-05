fd67c63bcbe2881af79163c5177bbb045e4630b5
Renan Maitan <renanmaitan12@gmail.com>
2024-04-04 21:13:21 -0300

    feat: auth.guard

--- ConstructionFlow.UI/src/app/app.routes.ts
+++ ConstructionFlow.UI/src/app/app.routes.ts
@@ -4,13 +4,14 @@ import { RegisterComponent } from '../_pages/register/register.component';
 import { LoginComponent } from '../_pages/login/login.component';
 import { ProfileComponent } from '../_pages/profile/profile.component';
 import { ConstructionComponent } from '../_pages/construction/construction.component';
+import { authGuard } from '../security/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'register', component:RegisterComponent},
     { path: 'login', component: LoginComponent},
-    { path: 'profile', component: ProfileComponent},
-    { path: 'construction', component: ConstructionComponent},
+    { path: 'profile', component: ProfileComponent, canActivate: [authGuard]},
+    { path: 'construction', component: ConstructionComponent, canActivate: [authGuard]},
     { path: '**', redirectTo: 'home' }
 ];
