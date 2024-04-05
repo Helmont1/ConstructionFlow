2241dd9adef1d838c21b4343314c7c06438c98fa
Renan Maitan <renanmaitan12@gmail.com>
2024-04-05 16:11:48 -0300

    fix: session to local

--- ConstructionFlow.UI/src/_pages/profile/profile.component.ts
+++ ConstructionFlow.UI/src/_pages/profile/profile.component.ts
@@ -24,7 +24,7 @@ export class ProfileComponent implements OnInit {
   }
   
   ngOnInit() {
-    this.user = JSON.parse(sessionStorage.getItem('user') ?? '{}');
+    this.user = JSON.parse(localStorage.getItem('user') ?? '{}');
   }
 
   createConstruction() {
