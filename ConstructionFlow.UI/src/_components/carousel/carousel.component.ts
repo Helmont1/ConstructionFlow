cd3caeb151b2b77c9d2c77e37036cd739e5abce5
Renan Maitan <renanmaitan12@gmail.com>
2024-04-15 14:42:39 -0300

    profile real data

--- ConstructionFlow.UI/src/_components/carousel/carousel.component.ts
+++ ConstructionFlow.UI/src/_components/carousel/carousel.component.ts
@@ -1,44 +1,42 @@
 import { CommonModule, NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
-
-export interface carouselItem {
-  constructionId: string,
-  status: {
-    statusName: string,
-    statusId?: string
-  }
-  endDate: Date,
-  startDate: Date,
-  customer: {
-    customerId?: string,
-    name: string, //-----------
-    cpf?: string,
-    cnpj?: string
-  },
-  title: string, //-------------
-  image: string,
-}
-
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [
-    NgClass,
-    CommonModule
-  ],
+  imports: [NgClass, CommonModule],
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss'
+  styleUrl: './carousel.component.scss',
 })
-export class CarouselComponent{
-
-  @Input() carouselItems: carouselItem[] = [];
+export class CarouselComponent {
+  @Input() carouselItems: any = [];
   @Input() indicators: boolean = true;
   @Input() controls: boolean = true;
   @Input() itemsPerSlide: number = 3;
-  
+  defaultImage = 'https://static.thenounproject.com/png/4595376-200.png';
+  // defaultImage = `data:image/jpeg;base64,/...`;
+
+  format(register: string): string {
+    if (!register || register.length !== 14) {
+      if (!register || register.length !== 11) {
+        return register;
+      }
+      return `${register.substring(0, 3)}.${register.substring(3, 6)}.${register.substring(
+        6,
+        9
+      )}-${register.substring(9)}`;
+    }
+    return `${register.substring(0, 2)}.${register.substring(2, 5)}.${register.substring(
+      5,
+      8
+    )}/${register.substring(8, 12)}-${register.substring(12)}`;
+  }
+
   selectedIndex = 0;
   getActiveClass(index: number): string {
-    if (index >= this.selectedIndex && index < this.selectedIndex + this.itemsPerSlide) {
+    if (
+      index >= this.selectedIndex &&
+      index < this.selectedIndex + this.itemsPerSlide
+    ) {
       return 'item-active';
     }
     return '';
@@ -53,17 +51,23 @@ export class CarouselComponent{
   }
 
   calculateNextIndexForZero(): number {
-   let rest = this.carouselItems.length % this.itemsPerSlide;
-   if (rest === 0) {
-     return this.carouselItems.length - this.itemsPerSlide;
-   }
-    return this.carouselItems.length - rest; 
+    let rest = this.carouselItems.length % this.itemsPerSlide;
+    if (rest === 0) {
+      return this.carouselItems.length - this.itemsPerSlide;
+    }
+    return this.carouselItems.length - rest;
   }
   onPrevClick(): void {
-    this.selectedIndex = this.selectedIndex === 0 ? this.calculateNextIndexForZero() : this.selectedIndex - this.itemsPerSlide;
+    this.selectedIndex =
+      this.selectedIndex === 0
+        ? this.calculateNextIndexForZero()
+        : this.selectedIndex - this.itemsPerSlide;
   }
 
   onNextClick(): void {
-    this.selectedIndex = this.selectedIndex >= this.carouselItems.length - this.itemsPerSlide ? 0 : this.selectedIndex + this.itemsPerSlide;
+    this.selectedIndex =
+      this.selectedIndex >= this.carouselItems.length - this.itemsPerSlide
+        ? 0
+        : this.selectedIndex + this.itemsPerSlide;
   }
 }
