8a97e0b57f4b585f29ef07d8bfb12a977be8e061
Renan Maitan <renanmaitan12@gmail.com>
2024-05-19 23:31:06 -0300

    feat: activity

--- ConstructionFlow.UI/src/_components/timeline/timeline.component.ts
+++ ConstructionFlow.UI/src/_components/timeline/timeline.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Activity } from '../../_models/activity.model';
+import { ActivityComponent } from '../activity/activity.component';
 
 @Component({
   selector: 'app-timeline',
   standalone: true,
-  imports: [],
+  imports: [
+    ActivityComponent
+  ],
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.scss'
 })
 export class TimelineComponent {
-
+  atividades: Activity[] = [];
 }
