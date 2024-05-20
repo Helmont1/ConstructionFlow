1511d041d2289320ad882d52da42b8d078e86c02
Renan Maitan <renanmaitan12@gmail.com>
2024-05-19 22:03:08 -0300

    ref: flow page design

--- ConstructionFlow.UI/src/_pages/flow/flow.component.ts
+++ ConstructionFlow.UI/src/_pages/flow/flow.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { LeftNavbarComponent } from '../../_components/left-navbar/left-navbar.component';
 import { NgClass } from '@angular/common';
+import { TimelineComponent } from '../../_components/timeline/timeline.component';
 
 @Component({
   selector: 'app-flow',
   standalone: true,
   imports: [
     LeftNavbarComponent,
-    NgClass
+    NgClass,
+    TimelineComponent
   ],
   templateUrl: './flow.component.html',
   styleUrl: './flow.component.scss'
 })
 export class FlowComponent {
-  page: string = 'timeline';
+  page: string = 'Linha do tempo';
 
   changePage(page: string) {
     this.page = page;
