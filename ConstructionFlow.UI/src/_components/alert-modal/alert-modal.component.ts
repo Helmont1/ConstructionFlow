b1d07b1c44d6adf870268cb864d9088a4a2f8613
Renan Maitan <renanmaitan12@gmail.com>
2024-06-05 20:32:51 -0300

    fix: make alert show more than one time

--- ConstructionFlow.UI/src/_components/alert-modal/alert-modal.component.ts
+++ ConstructionFlow.UI/src/_components/alert-modal/alert-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-alert-modal',
@@ -9,4 +9,5 @@ import { Component, Input } from '@angular/core';
 })
 export class AlertModalComponent {
   @Input() show: boolean = false;
+  @Output() closed = new EventEmitter<boolean>();
 }
