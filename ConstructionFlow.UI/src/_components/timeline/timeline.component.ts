31c248727c197d71b68b433e25cb9f0435ddc42f
Renan Maitan <renanmaitan12@gmail.com>
2024-06-05 18:43:45 -0300

    delayed construction

--- ConstructionFlow.UI/src/_components/timeline/timeline.component.ts
+++ ConstructionFlow.UI/src/_components/timeline/timeline.component.ts
@@ -5,14 +5,11 @@ import { ProgressCircleComponent } from '../progress-circle/progress-circle.comp
 @Component({
   selector: 'app-timeline',
   standalone: true,
-  imports: [
-    ActivityComponent,
-    ProgressCircleComponent
-  ],
+  imports: [ActivityComponent, ProgressCircleComponent],
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.scss',
 })
-export class TimelineComponent implements OnInit{
+export class TimelineComponent implements OnInit {
   @Input() atividades: Activity[] = [];
   budget = 0;
   Intl = Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
@@ -22,29 +19,30 @@ export class TimelineComponent implements OnInit{
   budgetPercentage = 0;
 
   ngOnInit(): void {
-    let finishedActivities = 0
-    this.atividades.forEach(atividade => {
+    let finishedActivities = 0;
+    this.atividades.forEach((atividade) => {
       atividade.startDate = new Date(atividade.startDate);
       atividade.endDate = new Date(atividade.endDate);
       if (atividade.budget) {
-        if (atividade.status!.id == 3)
-          this.budget += atividade.budget;
-        else if (atividade.status!.id == 2)
-          this.budget += atividade.budget/2;
+        if (atividade.status!.id == 3) this.budget += atividade.budget;
+        else if (atividade.status!.id == 2) this.budget += atividade.budget / 2;
       }
       if (atividade.status!.id == 3) {
         finishedActivities++;
-      }
-      else if (atividade.status!.id == 2) {
+      } else if (atividade.status!.id == 2) {
         finishedActivities += 0.5;
       }
     });
-    this.conclusionPercentage = Math.round((finishedActivities / this.atividades.length) * 10000)/100;
-    this.budgetPercentage = Math.round((this.budget / this.construction.budget) * 10000)/100;
+    this.conclusionPercentage =
+      Math.round((finishedActivities / this.atividades.length) * 10000) / 100;
+    this.budgetPercentage =
+      Math.round((this.budget / this.construction.budget) * 10000) / 100;
     this.endExpectedIn = this.getEndExpectedIn();
   }
 
   getEndExpectedIn() {
+    if (this.construction.status.id == 3) return 'Concluído';
+
     const endDate = this.construction.endDate;
     const now = new Date();
     const end = new Date(endDate);
@@ -55,13 +53,35 @@ export class TimelineComponent implements OnInit{
 
     // Ajustar os valores para evitar números negativos
     if (days < 0) {
-        months--;
-        days += new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
+      months--;
+      days += new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
     }
 
     if (months < 0) {
+      years--;
+      months += 12;
+    }
+
+    if (
+      years < 0 ||
+      (years === 0 && months < 0) ||
+      (years === 0 && months === 0 && days < 0)
+    ) {
+      years = now.getFullYear() - end.getFullYear();
+      months = now.getMonth() - end.getMonth();
+      days = now.getDate() - end.getDate();
+
+      if (days < 0) {
+        months--;
+        days += new Date(end.getFullYear(), end.getMonth() + 1, 0).getDate();
+      }
+
+      if (months < 0) {
         years--;
         months += 12;
+      }
+
+      return `Atrasado em ${years} anos ${months} meses e ${days} dias`;
     }
 
     return `${years} anos ${months} meses e ${days} dias`;
@@ -70,20 +90,21 @@ export class TimelineComponent implements OnInit{
   hasTimeLine(atividade: Activity, atividadeNext: Activity) {
     if (!atividadeNext) return false;
     return (
-      new Date(atividade.startDate).getFullYear() != new Date(atividadeNext.startDate).getFullYear()
+      new Date(atividade.startDate).getFullYear() !=
+      new Date(atividadeNext.startDate).getFullYear()
     );
   }
 
-  getData(atividade: Activity){
+  getData(atividade: Activity) {
     return {
       year: new Date(atividade.startDate).getFullYear(),
       date:
         '' +
-        this.getMonthName( new Date(atividade.startDate).getMonth()) +
+        this.getMonthName(new Date(atividade.startDate).getMonth()) +
         ' ' +
         new Date(atividade.startDate).getDate(),
-      name: atividade.activityName
-    }
+      name: atividade.activityName,
+    };
   }
 
   getMonthName(month: number) {
